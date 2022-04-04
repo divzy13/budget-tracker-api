6a044a8ebab744e32457197ed3c1b248cca320f3
divzy13 <divzy13@gmail.com>
2022-04-04 13:25:55 +0200

    Updated Budget tracker for Push

--- budgettracker_db.sql
+++ budgettracker_db.sql
@@ -1,28 +1,27 @@
 drop database trackerdb;
 drop user tracker;
-
 create user tracker with password 'password';
 create database trackerdb with template=template0 owner=tracker;
 \connect trackerdb;
 alter default privileges grant all on tables to tracker;
 alter default privileges grant all on sequences to tracker;
 
-create table bt_user {
+create table bt_user(
 user_id integer primary key not null,
 first_name varchar(20) not null,
 last_name varchar(20) not null,
 email varchar(20) not null,
 password text not null
-};
+);
 
-create table bt_category {
+create table bt_category(
 category_id integer primary key not null,
 user_id integer not null,
 title varchar(50) not null,
-description varchar(50) not null,
-};
+description varchar(50) not null
+);
 
-alter table bt_category add constrains cat_user_fk
+alter table bt_category add constraint cat_user_fk
 foreign key (user_id) references bt_user(user_id);
 
 create table bt_transactions(
@@ -34,10 +33,10 @@ note varchar(50) not null,
 transaction_date bigint not null
 );
 
-alter table bt_transactions add constrains trans_cat_fk
-foreign key category_id references bt_category(category_id);
-alter table bt_transactions add constrains trans_user_fk
-foreign key user_id references bt_user(user_id);
+alter table bt_transactions add constraint trans_cat_fk
+foreign key (category_id) references bt_category(category_id);
+alter table bt_transactions add constraint trans_user_fk
+foreign key (user_id) references bt_user(user_id);
 
 create sequence bt_user_seq increment 1 start 1;
 create sequence bt_category increment 1 start 1;
--- src/main/java/com/budgettracker/budgettrackerapi/BudgetController.java
+++ src/main/java/com/budgettracker/budgettrackerapi/BudgetController.java
@@ -1,18 +1,12 @@
 package com.budgettracker.budgettrackerapi;
 
-import com.budgettracker.budgettrackerapi.model.Transactions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class BudgetController {
-
-    @Autowired
-    public BudgetService budgetService;
 
     @PostMapping("/user")
     public String postUser(@RequestBody Map<String, Object> userMap) {
@@ -21,10 +15,5 @@ public class BudgetController {
         String email = (String) userMap.get("email");
         return firstname + " , " + lastname + " , " + email;
     }
-
-//    @GetMapping("/budget")
-//    public List<Transactions> getAllBudget() {
-//        return budgetService.getAllBudget();
-//    }
 
 }
